d2040e740811a29213e4805ce1681e2d38c74f64
chetanbakshi <chetan.bakshi@gmail.com>
2023-02-26 19:47:47 +0530

    added router

--- src/app/guards/profile-details.guard.ts
+++ src/app/guards/profile-details.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { BehaviorSubject} from 'rxjs';
 import { ProfileDetails } from '../interface/profile-details';
 import { AppService } from '../services/app.service';
@@ -8,11 +8,14 @@ import { AppService } from '../services/app.service';
   providedIn: 'root'
 })
 export class ProfileDetailsGuard implements Resolve<BehaviorSubject<ProfileDetails>> {
-  constructor(private service: AppService) {}
+  constructor(private service: AppService, private router: Router) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): BehaviorSubject<ProfileDetails> {
-      this.service.getProfileDetails().subscribe(res => console.log(res))
+      this.service.getProfileDetails().subscribe(res => {
+        console.log(res);
+        // this.router.navigate(['home'])
+      })
     return this.service.getProfileDetails()
   }
   
